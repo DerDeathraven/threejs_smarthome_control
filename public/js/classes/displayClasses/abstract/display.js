9e22e636da6b9546f924e3691220072777ae93d0
Deathraven <Lucky43@me.com>
2022-06-06 14:24:32 +0200

    some housekeeping

---
+++ public/js/classes/displayClasses/abstract/display.js
@@ -0,0 +1,22 @@
+export class Display {
+    constructor(name,className,domHook,hidden){
+        this.name = name;
+        this.className = className;
+
+        this.domHook = domHook;
+        this.hidden = hidden; 
+
+        this.generateDomElement()
+    }
+    update(){
+
+    }
+    mouseEnter(){
+
+    }
+    mouseLeave(){
+        
+    }
+
+    /*Generator Helper Functions */
+}
